e90c82384d4b77a03741d9ebc74701b729494086
shininglove <officialshineslove@gmail.com>
2023-09-24 17:34:43 -0400

    added show file and scroll

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 use tide::http::mime;
-use tide::prelude::*;
 use tide::utils::After;
 use tide::{log, Request, Response};
+use tide::{prelude::*, Body, StatusCode};
 use tide_jsx::html::HTML5Doctype;
 use tide_jsx::{component, html, rsx, view};
+use urlencoding::decode;
 
 #[component]
 fn Heading<'title>(title: &'title str) {
@@ -21,15 +22,31 @@ fn SearchInput(value: String) {
     rsx! {
         <input
             type={"search"}
+            name={"destination"}
             class={"p-2 border-2 border-pink-500 rounded w-1/3"}
+            hx-post={"/select_location"}
+            hx-trigger={"click from:#update"}
             value={value}
         />
     }
 }
 
 #[component]
-fn DirItem(value: String) {
-    let vals = format!(r#"{{"destination":"{}"}}"#, value);
+fn DirItem(value: PathBuf, parent: bool) {
+    let path = if !parent {
+        value
+            .file_stem()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or_default()
+    } else {
+        value.to_str().unwrap_or_default()
+    };
+    let vals = format!(r#"{{"destination":"{}"}}"#, path);
+    let destination = match parent {
+        true => "..".to_string(),
+        false => path.to_string(),
+    };
     rsx! {
         <div
           class={"text-2xl cursor-pointer"}
@@ -38,13 +55,14 @@ fn DirItem(value: String) {
           hx-trigger={"click"}
           hx-swap={"none"}
         >
-        {"📂"} {value}
+        {"📂"} {destination}
         </div>
     }
 }
 
 #[component]
 fn FileItem(value: PathBuf) {
+    // bug: everything after # symbol gets removed (state can solve this)
     let filename = value
         .file_stem()
         .unwrap_or_default()
@@ -55,10 +73,24 @@ fn FileItem(value: PathBuf) {
         .unwrap_or_default()
         .to_str()
         .unwrap_or_default();
+    let path = value.as_path().to_string_lossy();
     let value = format!("{}.{}", filename, extension);
+    let emoji = match extension.to_lowercase().as_str() {
+        "mp4" | "mov" => "📀",
+        "png" | "jpg" | "jpeg" => "📷",
+        _ => "❓",
+    };
+    let vals = format!(r#"{{"destination":"{}"}}"#, path);
     rsx! {
-        <div class={"text-2xl cursor-pointer"}>
-        {"📀"} {value}
+        <div
+        class={"text-2xl cursor-pointer"}
+        hx-post={"/show"}
+        hx-vals={vals}
+        hx-trigger={"click"}
+        hx-target={"#images"}
+        hx-swap={"innerHTML show:bottom"}
+        >
+            {emoji} {value}
         </div>
     }
 }
@@ -66,13 +98,22 @@ fn FileItem(value: PathBuf) {
 #[component]
 fn Image<'src>(src: &'src str) {
     rsx! {
-        <img class={"pt-2"} src={src} alt={"server-image"}/>
+        <img class={"pt-2 w-48 h-48"} src={src} alt={"server-image"}/>
+    }
+}
+
+#[component]
+fn Video<'src>(src: &'src str, size: &'src str) {
+    rsx! {
+    <video controls={"true"} width={size} height={size}>
+        <source src={src}></source>
+    </video>
     }
 }
 
 async fn dirs(req: Request<()>) -> tide::Result {
     let current: String = req.session().get("dir").unwrap_or_default();
-    let dir_names = fs::read_dir(current).unwrap();
+    let dir_names = fs::read_dir(&current).unwrap();
     let folders: Vec<_> = dir_names
         .into_iter()
         .filter(|d| {
@@ -83,36 +124,38 @@ async fn dirs(req: Request<()>) -> tide::Result {
                 .unwrap()
                 .to_str()
                 .unwrap()
-                .starts_with(".");
+                .starts_with('.');
             !dotfiles
         })
         .collect();
-    let directories: Vec<_> = folders
+    let mut directories: Vec<_> = folders
         .iter()
         .filter(|d| d.as_ref().unwrap().metadata().unwrap().is_dir())
         .map(|d| DirItem {
-            value: d
-                .as_ref()
-                .unwrap()
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string(),
+            value: d.as_ref().unwrap().path(),
+            parent: false,
         })
         .collect();
+    let path = Path::new(current.as_str());
+    let base_path = match path.parent() {
+        Some(p) => p,
+        None => path,
+    };
+    let parent = DirItem {
+        value: base_path.to_path_buf(),
+        parent: true,
+    };
+    directories.splice(0..0, vec![parent]);
     let files: Vec<_> = folders
         .iter()
         .filter(|d| {
             let item = d.as_ref().unwrap();
             let metadata = item.metadata().unwrap();
             let extension = match item.path().extension() {
-                Some(e) => match e.to_ascii_lowercase().to_str().unwrap() {
-                    "mp4" | "mov" => true,
-                    "png" | "jpg" | "jpeg" => true,
-                    _ => false,
-                },
+                Some(e) => matches!(
+                    e.to_ascii_lowercase().to_str().unwrap(),
+                    "mp4" | "mov" | "png" | "jpg" | "jpeg"
+                ),
                 None => false,
             };
             let is_file = metadata.is_file();
@@ -124,7 +167,7 @@ async fn dirs(req: Request<()>) -> tide::Result {
         .collect();
     view! {
         <>
-            <section class={"p-2 inline-flex flex-wrap gap-3"}>
+            <section class={"p-2 inline-flex flex-wrap gap-3 w-screen"}>
                 {directories}
             </section>
             <section class={"p-2 inline-flex flex-wrap gap-3"}>
@@ -145,6 +188,26 @@ async fn search(req: Request<()>) -> tide::Result {
     }
 }
 
+#[derive(Deserialize)]
+struct Location {
+    destination: String,
+}
+
+async fn showing(mut req: Request<()>) -> tide::Result {
+    let home_dir = std::env!("HOME").to_string();
+    let Location { destination } = req.body_form().await?;
+    let base_file = destination.replace(&home_dir, "/files");
+    match Path::new(destination.as_str())
+        .extension()
+        .unwrap_or_default()
+        .to_str()
+        .unwrap_or_default()
+    {
+        "mp4" | "mov" => view! {  <Video src={base_file.as_str()} size={"400"}/> },
+        _ => view! {<Image src={base_file.as_str()}/>},
+    }
+}
+
 async fn example(_req: Request<()>) -> tide::Result {
     let actual = html! {
         <Image src={"/files/Pictures/7b7.png"}/>
@@ -154,11 +217,6 @@ async fn example(_req: Request<()>) -> tide::Result {
         .header("x-api-key", "pandamonium")
         .body(actual)
         .build())
-}
-
-#[derive(Deserialize)]
-struct Location {
-    destination: String,
 }
 
 async fn update_dir_state(mut req: Request<()>) -> tide::Result {
@@ -206,12 +264,14 @@ async fn index(mut req: Request<()>) -> tide::Result {
                 >
                 <output id={"search_input"}>{""}</output>
               </section>
+              <section id={"images"} class={"p-1"}>{""}</section>
               <section class={"px-2 gap-2 flex"} id={"controls"}>
                 <button
+                  hx-get={"/data"}
+                  hx-target={"#data"}
                   class={"border-2 border-purple-500 p-2 rounded text-white font-extrabold bg-purple-400"}
-                  hx-get={"/test"}
-                  hx-target={"#test"}>
-                      {"Toggle Move"} </button>
+                  >
+                      {"Rename"} </button>
                 <button
                     class={"border-2 border-red-500 p-2 rounded text-white font-extrabold bg-pink-400"}
                     hx-get={"/example"}
@@ -220,12 +280,11 @@ async fn index(mut req: Request<()>) -> tide::Result {
                 </button>
                 <button
                     class={"border-2 border-lime-500 p-2 rounded text-white font-extrabold bg-lime-400"}
-                    hx-get={"/data"}
-                    hx-target={"#data"}>
-                        {"Rename"}
+                    id={"update"}
+                    >
+                        {"Update"}
                 </button>
               </section>
-            <section id={"images"} class={"p-1"}>{""}</section>
             <section
                 id={"files"}
                 hx-get={"/dirs"}
@@ -244,7 +303,6 @@ async fn index(mut req: Request<()>) -> tide::Result {
 #[tokio::main]
 async fn main() -> tide::Result<()> {
     log::start();
-    let home_dir = std::env!("HOME");
     let mut app = tide::new();
     app.with(tide::log::LogMiddleware::new());
     app.with(tide::sessions::SessionMiddleware::new(
@@ -262,9 +320,21 @@ async fn main() -> tide::Result<()> {
     }));
     app.at("/").get(index);
     app.at("/search").get(search);
-    app.at("/files").serve_dir(home_dir)?;
+    app.at("/files/*").get(|req: Request<()>| async move {
+        let home_dir = std::env!("HOME");
+        let path = req.url().path();
+        let file_location = decode(path)?;
+        let dir = file_location.to_string();
+        let file_dir = dir.replace("/files", home_dir);
+        if let Ok(body) = Body::from_file(file_dir).await {
+            Ok(body.into())
+        } else {
+            Ok(Response::new(StatusCode::NotFound))
+        }
+    });
     app.at("/static").serve_dir("./static")?;
     app.at("/example").get(example);
+    app.at("/show").post(showing);
     app.at("/dirs").get(dirs);
     app.at("/select_location").post(update_dir_state);
     app.listen("0.0.0.0:5000").await?;
